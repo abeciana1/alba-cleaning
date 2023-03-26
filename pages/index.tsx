25e3da21511d376780dde20765b2fc201e7eecad
Alex <alex.beciana@gmail.com>
2023-03-26 09:02:05 -0400

    Implemented NextSeo comp

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,14 @@
 import { NextSeo } from 'next-seo';
 
 export default function Home() {
+
   return (
     <>
+      <NextSeo
+        title="Alba Cleaning Service"
+        description="Sample text"
+        canonical=""
+      />
     </>
   )
 }
