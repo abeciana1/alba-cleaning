9d5307c292863fb91e1be93697e416f30af02791
Alex <alex.beciana@gmail.com>
2023-03-25 22:19:18 -0400

    Removed unused imports and code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,8 @@
-import Head from 'next/head'
-import Image from 'next/image'
-// import { Inter } from 'next/font/google'
-// import styles from '@/styles/Home.module.css'
 
-// const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <>
-      <h1>Hello world</h1>
-      <h2>Hello world</h2>
-      <h3>Hello world</h3>
-      <h4>Hello world</h4>
-      <h5>Hello world</h5>
-      <h6>Hello world</h6>
     </>
   )
 }
