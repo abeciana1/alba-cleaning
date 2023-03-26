75bad0644407fa8e74eadf1fd9df35e96f108289
Alex <alex.beciana@gmail.com>
2023-03-26 16:31:42 -0400

    Added favicon link tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/soap-suds-favicon.webp"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/soap-suds-favicon.webp"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/soap-suds-favicon.webp"/>
+        <link rel="mask-icon" href="/soap-suds-favicon.webp" color="#212121"/>
+      </Head>
       <body>
         <Main />
         <NextScript />
