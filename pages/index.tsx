2576a30a9182cf7a22d34b5025406fc08e8b438e
Alex <alex.beciana@gmail.com>
2023-03-27 18:58:17 -0400

    Added remote image pattern to next config file

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,7 +27,7 @@ export default function Home() {
         canonical=""
       />
       <HeroSection
-        title="Let's work together"
+        title="Let's get cleaning!"
         body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip."
         images={heroImages}
       />
