690ac0869458bb4c62282f4bce898e17797ae954
Alex <alex.beciana@gmail.com>
2023-03-30 18:08:29 -0400

    Removed ImageCompare interface

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -17,10 +17,6 @@ export interface TopHeroSectionI {
     image: SliderImageT;
 }
 
-export interface ImageCompareI {
-    images: SliderImageT[]
-}
-
 export interface QuoteI {
     quote: string;
 }
