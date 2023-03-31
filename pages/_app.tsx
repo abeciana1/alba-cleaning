b542c95d2ac34e5ebf6922b1c00963c15326027e
Alex <alex.beciana@gmail.com>
2023-03-31 18:40:41 -0400

    Added fetch options

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,22 @@ import type { AppProps } from 'next/app'
 import { NavBar } from '@/components/utils/Navigation'
 import { Client, Provider, cacheExchange, fetchExchange } from 'urql';
 
+const client = new Client({
+  url: 'http://localhost:3000/graphql',
+  exchanges: [cacheExchange, fetchExchange],
+  fetchOptions: () => {
+    const token = getToken();
+    return {
+      headers: { authorization: token ? `Bearer ${token}` : '' },
+    };
+  },
+});
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <Provider value={client}>
       <NavBar/>
       <Component {...pageProps} />
-    </>
+    </Provider>
   )
 }
