5fb1d0a0d2395a5975bf91d14602e782131b2348
Alex <alex.beciana@gmail.com>
2023-04-01 10:51:27 -0400

    Added date formating for copyright symbol and spacing

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,7 +20,8 @@ import { ServiceT } from '@/types'
 import Image from 'next/image'
 
 export default function Home({ serviceCleanings }: CleaningServicesI) {
-
+  const date = new Date()
+  let currentYear = date.getFullYear()
   const [currentQuote, setQuote] = useState<string>("")
 
   const randomIndexNum = (): number => {
@@ -135,19 +136,20 @@ export default function Home({ serviceCleanings }: CleaningServicesI) {
             quote={currentQuote}
           />
         </TwoColGrid>
-        <footer className="pt-10 md:pt-20">
+        <footer className="pt-10 md:pt-20 flex justify-center space-x-2">
+          &#169;{currentYear}
           <a
             href="https://alexbeciana.com/"
             target="_blank"
           >
-            <span className="flex items-center justify-center">
+            <span className="flex items-center justify-center ml-1">
               Created by
                 <Image
                   src="https://media.graphassets.com/output=format:webp/Bwz9zHRxS1S79v3Ppax0"
                   alt="AB Creative logo"
                   width={896}
                   height={570}
-                  className="w-6 ml-2"
+                  className="w-6 ml-1"
                 />
             </span>
           </a>
