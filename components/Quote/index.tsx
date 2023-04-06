c95a354aa9c7f0422728580c1a5b151603d365dd
Alex <alex.beciana@gmail.com>
2023-04-06 01:20:10 -0400

    Added lg styling on Quote comp

--- components/Quote/index.tsx
+++ components/Quote/index.tsx
@@ -4,7 +4,7 @@ import { QuoteI } from '@/interfaces'
 export const TipQuote: React.FC<QuoteI> = ({ quote }) => {
 
     return (
-        <section className="bg-background rounded-xl shadow-md">
+        <section className="bg-background rounded-xl shadow-md lg:mx-36">
             <blockquote className="text-xl p-10 text-center">
                 { quote }
             </blockquote>
